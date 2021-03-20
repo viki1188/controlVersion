d3cdcfb80af34f8a602994d15d8e61f5e45ba714
viki1188 <vikichavan81@rediffmail.com>
2021-03-20 20:35:19 +0530

    change again title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'controlVersion for';
+  title = 'controlVersion for hello viki';
 }
